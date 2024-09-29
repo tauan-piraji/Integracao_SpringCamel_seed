a82b0705d75c8a3b5bec990dfda392084c73da5a
piraji <tauan-oliv@hotmail.com>
2024-09-29 20:54:20 -0300

    finalizando

--- local.sh
+++ local.sh
@@ -7,4 +7,4 @@ echo 'Remove RabbitMQ'
 docker rm rabbitmq
 
 echo 'Start RabbitMQ'
-docker run -d -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=admin -e RABBITMQ_MAX_MESSAGE_SIZE=536870912 -p 15672:15672 -p 5672:5672 --hostname rabbitmq --name rabbitmq rabbitmq:3-management 
+docker run -d -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=admin -e RABBITMQ_MAX_MESSAGE_SIZE=536870912 -p 15777:15777 -p 5677:5677 --hostname rabbitmq --name rabbitmq rabbitmq:3-management
